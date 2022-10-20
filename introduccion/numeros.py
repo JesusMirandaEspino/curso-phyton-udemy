834e2daf4f9fe6e7ecc435948b19a6a8955f1812
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-10-20 11:03:05 -0500

    numeros

---
+++ introduccion/numeros.py
@@ -0,0 +1,43 @@
+#Suma
+print(3 + 2 )
+# Resta
+print(3 - 2)
+# Multiplicacion
+print(3 * 2)
+# Division
+print(6 / 2)
+# Residuo
+print(3 % 2)
+# Potencia
+print(3 ** 2)
+
+print(3 - 2 + 4 * 10)
+
+
+n = 3
+print(n)
+print(n + 3)
+print(n * 2)
+print(n * n)
+
+# Creamos otra variable
+m = 10
+
+print(n + m)
+print(n * m + 10)
+
+n = 10
+m = 15
+print(n + m)
+
+n = m
+print(n)
+
+n = m + 10
+n = n + 25
+
+nota_1 = 2
+nota_2 = 5
+nota_media = (nota_1 + nota_2) / 2
+
+print(nota_media)
