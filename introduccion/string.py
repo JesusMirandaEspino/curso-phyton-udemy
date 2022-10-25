f0ae7e486916e832f94e3577a810a1641cabeaca
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-10-25 10:22:28 -0500

    strings indices

--- introduccion/string.py
+++ introduccion/string.py
@@ -19,3 +19,29 @@ cadena3 = " es un lenguaje"
 
 # completa el ejercicio
 cadena5 = cadena2 + cadena3 + cadena4 + cadena1
+
+
+palabra = "Python"
+palabra[0]  # carácter en la posición 0
+
+
+print(palabra[0])
+print(palabra[3])
+print(palabra[-0])
+print(palabra[-2])
+print(palabra[-6])
+
+
+
+#slices
+print(palabra[0:2])
+print(palabra[2:])
+print(palabra[-2:])
+print(palabra[:2])
+print(palabra[:2] + palabra[2:])
+print(palabra[:])
+print(palabra[:99])
+print(palabra[99:])
+
+
+# Error print(palabra[99])
