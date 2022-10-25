64854625628f0fffe83fe046b1078c3201459526
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-10-25 10:32:58 -0500

    listas

---
+++ introduccion/listas.py
@@ -0,0 +1,40 @@
+numeros = [1, 2, 3, 4]
+
+
+datos = [4, "Una cadena", -15, 3.14, "Otra cadena"]
+
+print(datos[0])
+print(datos[-1])
+print(datos[2:])
+
+
+print(numeros + [5,6,7,8])
+
+
+pares = [0, 2, 4, 5, 8, 10]
+pares[3] = 6
+
+pares.append(12)
+pares.append(7*2)
+
+
+print(pares)
+
+
+letras = ['a', 'b', 'c', 'd', 'e', 'f']
+letras[:3]
+print(letras)
+
+letras[:3] = ['A','B','C']
+
+print(letras)
+
+
+letras[:3] = []
+print(letras)
+
+letras = []
+print(letras)
+
+print(len(letras))
+print(len(pares))
