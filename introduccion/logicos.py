151ad11f79d9104556c8936e447a73d9fbb6dbaf
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-10-27 09:47:54 -0500

    logicos

---
+++ introduccion/logicos.py
@@ -0,0 +1,45 @@
+print(1 + 1 == 3)
+print(1 + 1 == 2)
+
+
+print(3 == 2)
+print(3 != 2)
+print(3 > 2)
+print(3 < 2)
+print(3 >= 4)
+print(3 <= 4)
+
+
+a = 10
+b = 5
+
+print(a > b)
+print(b != a)
+print(a == b*2)
+print("Hola" == "Hola")
+print("Hola" != "Hola")
+
+c = "Hola"
+print(c[0] == "H")
+
+c = "Hola"
+print(c[-1] == "a")
+
+
+l1 = [0, 1, 2]
+l2 = [2, 3, 4]
+
+print(l1 == l2)
+print(len(l1) == len(l2))
+print(l1[-1] == l2[0])
+print(True == True)
+print(False == True)
+print(False != True)
+print(True > False)
+print(False > True)
+
+print(True * 3)
+print(False / 5)
+print(True * False)
+
+
