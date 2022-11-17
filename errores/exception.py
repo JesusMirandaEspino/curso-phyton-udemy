9f66646644a3b584751554098530cec02fb0e6b2
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-11-17 10:20:14 -0600

    rise

--- errores/exception.py
+++ errores/exception.py
@@ -147,3 +147,30 @@ except ZeroDivisionError:
     print("No se puede dividir por cero, prueba otro número")
 except Exception as e:
     print("Ha ocurrido un error no previsto", type(e).__name__)
+
+
+# Invocación de excepciones
+# En algunas ocasiones quizá nos interesa llamar un error manualmente, ya que un print común no es muy elegante:
+
+
+def mi_funcion(algo=None):
+    if algo is None:
+        print("Error! No se permite un valor nulo (con un print)")
+
+
+mi_funcion()
+
+# Error! No se permite un valor nulo(con un print)
+# Instrucción raise
+# Gracias a raise podemos lanzar un error manual pasándole el identificador. Luego simplemente podemos añadir un except para tratar esta excepción que hemos lanzado:
+
+
+def mi_funcion(algo=None):
+    try:
+        if algo is None:
+            raise ValueError("Error! No se permite un valor nulo")
+    except ValueError:
+        print("Error! No se permite un valor nulo (desde la excepción)")
+
+
+mi_funcion()
