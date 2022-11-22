c6fdd00bc968f783427dfae3188db5f3c27795a9
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-11-22 09:29:00 -0600

    inside object

--- poo/poo.py
+++ poo/poo.py
@@ -125,3 +125,44 @@ empresa.borrar_cliente("22222222B")
 # Muestro de nuevo todos los clientes
 print("\n==LISTADO DE CLIENTES==")
 print(empresa.clientes)
+
+
+# Objetos dentro de objetos
+# Hasta ahora no lo hemos comentado, pero al ser las clases un nuevo tipo de dato resulta 
+# más que obvio que se pueden poner en colecciones e incluso utilizarlos dentro de otras clases.
+# Os voy a dejar un pequeño código de ejemplo sobre un catálogo de películas para que lo estudiéis detenidamente:
+
+
+class Pelicula:
+
+    # Constructor de clase
+    def __init__(self, titulo, duracion, lanzamiento):
+        self.titulo = titulo
+        self.duracion = duracion
+        self.lanzamiento = lanzamiento
+        print('Se ha creado la película:', self.titulo)
+
+    def __str__(self):
+        return '{} ({})'.format(self.titulo, self.lanzamiento)
+
+
+class Catalogo:
+
+    peliculas = []  # Esta lista contendrá objetos de la clase Pelicula
+
+    def __init__(self, peliculas=[]):
+        self.peliculas = peliculas
+
+    def agregar(self, p):  # p será un objeto Pelicula
+        self.peliculas.append(p)
+
+    def mostrar(self):
+        for p in self.peliculas:
+            print(p)  # Print toma por defecto str(p)
+
+
+p = Pelicula("El Padrino", 175, 1972)
+c = Catalogo([p])  # Añado una lista con una película desde el principio
+c.mostrar()
+c.agregar(Pelicula("El Padrino: Parte 2", 202, 1974))  # Añadimos otra
+c.mostrar()
