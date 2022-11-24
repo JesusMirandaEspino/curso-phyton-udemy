f2167a13551cc7c96b777760d13e41df824939cb
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-11-24 08:08:56 -0600

    cuadrante parte 2

--- ejercicios/plano.py
+++ ejercicios/plano.py
@@ -13,4 +13,16 @@ class punto:
             print("{} Pertenece al primer cuadrante".format(self))
         elif self.x < 0 and self.y > 0:
             print("{} Pertenece al segundo cuadrante".format(self))
-    
+        elif self.x < 0 and self.y < 0:
+            print("{} Pertenece al tercer cuadrante".format(self))
+        elif self.x > 0 and self.y < 0:
+            print("{} Pertenece al cuarto cuadrante".format(self))
+        else:
+            print("{} Se encuentra sobre el origen".format(self))
+
+    def vectores(self, p):
+        print("El vector entre {} y {} es ({}, {})".format(self, p, p.x - self.x, p.y - self.y))
+
+    def distancia(self, p):
+        d = math.sort( (p.x - self.x)**2 + (p.y - self.y)**2 )
+        print("La distancia entre los puentos {} y {} es {}".format(self, p, d))
