1c0d978a0d81edac5c651b263e70c973011858c2
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-12-15 07:51:07 -0600

    pickle part2

--- ficheros/pickle.py
+++ ficheros/pickle.py
@@ -1,3 +1,4 @@
+from io import open
 import pickle
 # Módulo pickle
 # Este módulo nos permite almacenar fácilmente colecciones y 
@@ -73,3 +74,111 @@ for p in personas:
 # Héctor
 # Mario
 # Marta
+
+
+# Ejemplo catálogo de películas
+# A continuación os dejo un ejemplo para que veáis como adaptar un catálogo de películas para usar persistencia utilizando pickle:
+
+
+class Pelicula:
+
+    # Constructor de clase
+    def __init__(self, titulo, duracion, lanzamiento):
+        self.titulo = titulo
+        self.duracion = duracion
+        self.lanzamiento = lanzamiento
+        print('Se ha creado la película:', self.titulo)
+
+    def __str__(self):
+        return '{} ({})'.format(self.titulo, self.lanzamiento)
+
+
+class Catalogo:
+
+    peliculas = []
+
+    # Constructor de clase
+    def __init__(self):
+        self.cargar()
+
+    def agregar(self, p):
+        self.peliculas.append(p)
+        self.guardar()
+
+    def mostrar(self):
+        if len(self.peliculas) == 0:
+            print("El catálogo está vacío")
+            return
+        for p in self.peliculas:
+            print(p)
+
+    def cargar(self):
+        fichero = open('catalogo.pckl', 'ab+')
+        fichero.seek(0)
+        try:
+            self.peliculas = pickle.load(fichero)
+        except:
+            print("El fichero está vacío")
+        finally:
+            fichero.close()
+            print("Se han cargado {} películas".format(len(self.peliculas)))
+
+    def guardar(self):
+        fichero = open('catalogo.pckl', 'wb')
+        pickle.dump(self.peliculas, fichero)
+        fichero.close()
+
+
+# El uso de las clases no tendría mucha complicación:
+
+    # Creamos un catálogo
+c = Catalogo()
+
+# Mostramos el contenido
+c.mostrar()
+
+# Agregamos unas películas
+c.agregar(Pelicula("El Padrino", 175, 1972))
+c.agregar(Pelicula("El Padrino: Parte 2", 202, 1974))
+
+# Mostramos el catálogo de nuevo
+c.mostrar()
+
+# Borramos el catálogo de la memoria ram (persistirá el fichero)
+del(c)
+
+# El fichero está vacío
+# Se han cargado 0 películas
+
+# El catálogo está vacío
+
+# Se ha creado la película: El Padrino
+# Se ha creado la película: El Padrino: Parte 2
+
+# El Padrino(1972)
+# El Padrino: Parte 2 (1974)
+# Ahora al crear de nuevo un catálogo se recuperarán los datos del fichero y 
+# podríamos seguir trabajando con él:
+
+    # Creamos un catálogo
+c = Catalogo()
+
+# Mostramos el contenido
+c.mostrar()
+
+# Agregamos una película
+c.agregar(Pelicula("Prueba", 100, 2005))
+
+# Mostramos el catálogo de nuevo
+c.mostrar()
+
+# Se han cargado 2 películas
+
+# El Padrino(1972)
+# El Padrino: Parte 2 (1974)
+
+# Se ha creado la película: Prueba
+
+# El Padrino(1972)
+# El Padrino: Parte 2 (1974)
+# Prueba(2005)
