e74b2d543a0f6a0122b0a2221c0a859a6cc21ac1
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-12-29 08:18:56 -0600

    menu part 2

--- menu.py
+++ menu.py
@@ -13,8 +13,12 @@ filemenu.add_command(label="Cerrar")
 filemenu.add_separator()
 filemenu.add_command(label="Salir", command=root.quit)
 
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Cortar")
+editmenu.add_command(label="Copiar")
+editmenu.add_command(label="Pegar")
 
 menubar.add_cascade(label="Archivo", menu=filemenu)
-
+menubar.add_cascade(label="Editar", menu=editmenu)
 
 root.mainloop()
