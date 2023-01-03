c0591de1f05e77c7125f344194553913554f20b7
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-01-03 08:23:37 -0600

    editor part 1

---
+++ interfaces/editor1.py
@@ -0,0 +1,23 @@
+from tkinter import *
+root = Tk()
+root.title("Mi editor")
+
+# Menú superior
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Nuevo")
+filemenu.add_command(label="Abrir")
+filemenu.add_command(label="Guardar")
+filemenu.add_command(label="Guardar como")
+filemenu.add_separator()
+filemenu.add_command(label="Salir", command=root.quit)
+menubar.add_cascade(label="Archivo", menu=filemenu)
+
+# Caja de texto central
+texto = Text(root)
+texto.pack(fill='both', expand=1)
+texto.config(padx=6, pady=4, bd=0, font=("Consolas", 12))
+
+# Menu y bucle de la aplicación
+root.config(menu=menubar)
+root.mainloop()
