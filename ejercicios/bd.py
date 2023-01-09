713e672ba91c129ea91e57e1b481eb68ec924856
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-01-09 10:07:50 -0600

    bd part 1

---
+++ ejercicios/bd.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+def crear_db():
+    conexion = sqlite3.connect('restaurante.db')
+    cursor = conexion.cursor()
+
+    try: 
+        cursor.execute(
+            ''' CREATE TABLE categoria( id INTEGER PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(100) UNIQUE NOT NULL ) ''')
+    except sqlite3.OperationalError:
+        print('la tabla categoria ya existe')
+    else:
+        print('La tabla categorias se ha creado correctamente')
+
+    try:
+        cursor.execute(
+            ''' CREATE TABLE plato( id INTEGER PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(100) UNIQUE NOT NULL, categoria_id INTEGER NOT NULL, FOREIGN KEY(categoria_id) REFERENCES categoria(id)) ''')
+    except sqlite3.OperationalError:
+        print('la tabla platos ya existe')
+    else:
+        print('La tabla platos se ha creado correctamente')
+
+
+    conexion.close()
+
+
+crear_db()
