c678458de9f567f1578b427fc41365ab965b3898
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-01-10 09:13:47 -0600

    menu part 3

--- ejercicios/menu.py
+++ ejercicios/menu.py
@@ -7,14 +7,25 @@ root.title('Bar Cheeto - Menu')
 root.resizable(0,0)
 root.config(bd=25,relief='sunken')
 
-Label(root, text='   Bar Cheeto   ', fg='darkgreen', font=( 'Times New Roman', 28, 'bold italic' ) ).pack()
-Label(root, text='   Menu del dia   ', fg='green', font=('Times New Roman', 24, 'bold italic')).pack()
+FONTTYPE = 'Times New Roman'
+FONTSTYLE = 'bold italic'
+
+Label(root, text='   Bar Cheeto   ', fg='darkgreen', font=(FONTTYPE, 28, FONTSTYLE)).pack()
+Label(root, text='   Menu del dia   ', fg='green', font=(FONTTYPE, 24, FONTSTYLE)).pack()
 Label(text='').pack()
 
 conexion = sqlite3.connect('restaurante.db')
 cursor = conexion.cursor()
 
+categorias = cursor.execute("SELECT * FROM categoria").fetchall()
 
+for categoria in categorias:
+    Label(root, text=categoria[1], fg='darkgreen', font=( 'Times New Roman', 20, 'bold italic')).pack()
+    platos = cursor.execute(
+        "SELECT * FROM plato WHERE categoria_id = {} ".format(categoria[0])).fetchall()
+    for plato in platos:
+        Label(root, text=plato[1], fg='gray', font=('Times New Roman', 16, 'bold italic')).pack()
+    Label(text='').pack()
 
 conexion.close()
 root.mainloop()
