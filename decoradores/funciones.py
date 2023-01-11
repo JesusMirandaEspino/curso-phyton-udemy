7f127b0db1a75d9f8b1cbe55c44878916b3b276c
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-01-11 09:12:54 -0600

    decoradores part 3

--- decoradores/funciones.py
+++ decoradores/funciones.py
@@ -14,3 +14,13 @@ def hola1():
         return "Hola!"
     print(locals())  # Mostramos el ámbito local
 hola1()
+
+
+# Antes de ejecutar el bloque reinicia el Notebook para vaciar la memoria
+lista2 = [1, 2, 3]
+def hola2():
+    numero = 50
+    def bienvenido2():
+        return "Hola!"
+    print(globals())  # Mostramos el ámbito global
+hola2()
