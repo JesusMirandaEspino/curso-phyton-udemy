1fa2d38e41f003b36e8ba8bcd28716f7246f19a2
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-01-11 09:12:31 -0600

    decoradores part 2

--- decoradores/funciones.py
+++ decoradores/funciones.py
@@ -1,11 +1,16 @@
 # Funciones decoradoras
 
 def hola():
-
     def bienvenido():
         return "Hola!"
-
     print(locals())  # Mostramos el ámbito local
-
-
 hola()
+
+
+lista = [1, 2, 3]
+def hola1():
+    numero = 50
+    def bienvenido1():
+        return "Hola!"
+    print(locals())  # Mostramos el ámbito local
+hola1()
