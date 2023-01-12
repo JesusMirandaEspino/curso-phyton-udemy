d07927918a1d8ff0e433b6528ea2f516a95eb319
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-01-12 08:21:31 -0600

    generadoras part 3

--- decoradores/generadoras.py
+++ decoradores/generadoras.py
@@ -6,6 +6,7 @@
 def pares(n):
     for numero in range(n+1):
         if numero % 2 == 0:
+            print(numero)
             yield numero
 pares(10)
 
@@ -13,3 +14,18 @@ for numero in pares(10):
     print(numero)
 
 [numero for numero in pares(10)]
+
+
+pares = pares(3)
+
+
+print(next(pares))
+print(next(pares))
+
+lista = [1, 2, 3, 4, 5]
+lista_iterable = iter(lista)
+print(next(lista_iterable))
+print(next(lista_iterable))
+print(next(lista_iterable))
+print(next(lista_iterable))
+print(next(lista_iterable))
