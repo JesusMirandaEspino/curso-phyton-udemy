617e6a436a1ea0e415864e7481446a826267e70e
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-01-12 08:36:21 -0600

    filter part 2

--- decoradores/filter.py
+++ decoradores/filter.py
@@ -12,3 +12,27 @@ print(list(filter(multiple, numeros)))
 nueva_lista = list(filter(lambda numero: numero % 5 == 0, numeros))
 
 print(nueva_lista)
+
+
+class Persona:
+
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def __str__(self):
+        return "{} de {} años".format(self.nombre, self.edad)
+
+
+personas = [
+    Persona("Juan", 35),
+    Persona("Marta", 16),
+    Persona("Manuel", 78),
+    Persona("Eduardo", 12)
+]
+
+
+menores = filter(lambda persona: persona.edad < 18, personas)
+
+for menor in menores:
+    print(menor)
