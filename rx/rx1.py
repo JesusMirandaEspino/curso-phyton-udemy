75917a1cff14176c081169314bebb18cb9d44655
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-01-13 08:23:42 -0600

    expresiones regulares parte 4

--- rx/rx1.py
+++ rx/rx1.py
@@ -35,5 +35,30 @@ print(re.sub('amigo', 'amiga', texto3))
 texto4 = "hola adios hola hola"
 print(re.findall('hola', texto4))
 
-
 print(len(re.findall('hola', texto4)))
+
+
+texto5 = "hola adios hello bye"
+print(re.findall('hola|hello', texto5))
+
+texto6 = "hla hola hoola hooola hooooola"
+
+def buscar(patrones, texto6):
+    for patron in patrones:
+        print(re.findall(patron, texto6))
+
+
+patrones = ['hla', 'hola', 'hoola']
+buscar(patrones, texto6)
+
+patrones1 = ['ho', 'ho*', 'ho*la', 'hu*la']
+buscar(patrones1, texto6)
+
+patrones2 = ['ho*', 'ho+']
+buscar(patrones2, texto6)
+
+patrones3 = ['ho*', 'ho+', 'ho?', 'ho?la']
+buscar(patrones3, texto6)
+
+patrones4 = ['ho{0,1}la', 'ho{1,2}la', 'ho{2,9}la']
+buscar(patrones4, texto6)
