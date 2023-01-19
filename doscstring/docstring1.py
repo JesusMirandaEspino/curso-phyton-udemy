e71beac7d0416b3e4db7079e3473e7ca7d4d505e
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-01-19 08:22:00 -0600

    docstring part 3

--- doscstring/docstring1.py
+++ doscstring/docstring1.py
@@ -1,3 +1,5 @@
 import mi_modulo
 
 help(mi_modulo)
+help(mi_modulo.despedir)
+dir(mi_modulo)
---
+++ doscstring/docstring3.py
@@ -0,0 +1,13 @@
+import mi_paquete
+import datetime
+
+help(datetime)
+
+help(mi_paquete)
+
+help(print)
+
+help(len)
+
+
+help(str)
