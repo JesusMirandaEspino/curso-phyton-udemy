b3384c255d1a64dd362cbfce6f8799908e89b1a5
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-01-19 08:47:52 -0600

    test part 2

--- test/test.py
+++ test/test.py
@@ -1,2 +1,28 @@
+import doctest
 
 
+def palindromo(palabra):
+    """
+    Comprueba si una palabra es un palíndrimo. Los palíndromos son
+    palabras o frases que se leen igual de ambos lados.
+    Si es un palíndromo devuelve True y si no False
+
+    >>> palindromo("radar")
+    True
+
+    >>> palindromo("somos")
+    True
+
+    >>> palindromo("holah")
+    False
+
+    >>> palindromo("Atar a la rata")
+    True
+    """
+    if palabra.lower().replace(" ", "") == palabra[::-1].lower().replace(" ", ""):
+        return True
+    else:
+        return False
+
+
+doctest.testmod()
