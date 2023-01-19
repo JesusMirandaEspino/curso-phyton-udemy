87936b102fe9d95c681003cd587c1b53148d6fa7
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-01-19 08:47:39 -0600

    test part 1

---
+++ test/test1.py
@@ -0,0 +1,17 @@
+import doctest
+def suma(a, b):
+    """Esta función recibe dos parámetros y devuelve la suma de ambos
+
+    >>> suma(5,10)
+    15
+
+    >>> suma(0,0)
+    1
+
+    >>> suma(-5,7)
+    2
+    """
+    return a+b
+
+
+doctest.testmod()
