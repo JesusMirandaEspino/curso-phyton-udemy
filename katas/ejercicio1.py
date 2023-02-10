d73a8f1b91fbb20169a3371f17f25d213c470731
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-02-10 08:46:34 -0600

    kata 2

--- katas/ejercicio1.py
+++ katas/ejercicio1.py
@@ -2,3 +2,10 @@ def make_negative(number):
     if number < 0:
         return number
     return -number
+
+
+def twice_as_old(dad_years_old, son_years_old):
+    return abs(dad_years_old - (son_years_old * 2))
+    
+
+
