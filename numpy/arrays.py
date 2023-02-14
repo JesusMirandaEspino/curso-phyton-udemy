7ca758d87792bfe2387dea7fc2481fdcf4f2537c
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-02-14 09:36:44 -0600

    arrays np

---
+++ numpy/arrays.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+print(np.zeros(3))
+print('-----------')
+print(np.zeros([3,3]))
+print('-----------')
+print(np.ones([3,3]))
+print('--------------')
+print(np.eye(3))
+print('--------------')
+
+print(np.arange(4))
+print('----------------')
+print(np.arange(4.))
+print('--------------')
+print(np.arange(-3, 3))
+print('-----------------')
+print(np.arange(0, 20, 5))
