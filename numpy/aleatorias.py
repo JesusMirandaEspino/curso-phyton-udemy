749b0bf1330da72725808914852f5d2963e7ee9e
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-02-16 09:30:35 -0600

    numpy operaciones aleatorias parte 2

--- numpy/aleatorias.py
+++ numpy/aleatorias.py
@@ -19,3 +19,25 @@ print(spaceChart)
 
 print(np.random.randint(10))
 print(spaceChart)
+
+print(np.random.randint(10, size=[3, 2]))
+print(spaceChart)
+
+print(np.random.randint(-10, 10, size=[3, 2]))
+print(spaceChart)
+
+print(np.random.normal(size=20))
+print(spaceChart)
+
+arr = np.arange(10)
+print(spaceChart)
+print(arr)
+print(spaceChart)
+
+np.random.shuffle(arr)
+print(arr)
+print(spaceChart)
+
+print(np.random.permutation(15))
+print(spaceChart)
+
