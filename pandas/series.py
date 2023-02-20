3b4174e87778f52a46f808b8c715f6f61016ea5f
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-02-20 09:25:12 -0600

    series parte 2

--- pandas/series.py
+++ pandas/series.py
@@ -18,9 +18,38 @@ print(espacio)
 array = np.random.randint(50, size=4)
 print(array)
 
+print(espacio)
+print(pd.Series(array))
 
-print()
-print()
+print(espacio)
+print(pd.Series(array, etiquetas))
+
+diccionario = {'A': 25, 'B': 50, 'C': 75, 'D': 100}
+print(espacio)
+print(pd.Series(diccionario))
+
+ingresos = pd.Series([100, 300, 200], index=['enero', 'febrero', 'marzo'])
+print(espacio)
+print(ingresos)
+
+print(espacio)
+print(ingresos[0])
+print(espacio)
+print(ingresos['enero'])
+
+gastos = pd.Series([100, 150, 250], index=['enero', 'febrero', 'marzo'])
+print(espacio)
+print(gastos)
+
+total = ingresos.subtract(gastos)
+print(espacio)
+print(total)
+
+print(espacio)
+print(ingresos - gastos)
+
+print(espacio)
+print(type(total))
 print()
 print()
 print()
