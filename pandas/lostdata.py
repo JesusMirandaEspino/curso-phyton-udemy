9c5de810dbe35f4425d2eca432aa4879c67c72f6
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-02-20 09:42:02 -0600

    lost data

--- pandas/lostdata.py
+++ pandas/lostdata.py
@@ -3,16 +3,32 @@ import numpy as np
 
 espacio = '***********************'
 
+df = pd.DataFrame({
+    'A': [7, 8, -6, 8],
+    'B': [12, np.nan, 1, np.nan],
+    'C': [4, np.nan, np.nan, np.nan],
+    'D': [4, np.nan, -2, -10]})
+
+print(df)
 print(espacio)
 
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
+print(df.isnull())
+print(espacio)
+
+print(df.dropna())
+print(espacio)
+
+print(df.dropna(axis=1))
+print(espacio)
+
+print(df.dropna(thresh=2))
+print(espacio)
+
+print(df.dropna(thresh=3))
+print(espacio)
+
+print(df.fillna(value=0))
+print(espacio)
+
+print(df['B'].fillna(value=df['B'].min()))
+print(espacio)
